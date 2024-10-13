d4b1fe64ee0d3d3452a7f6aeeed7bc850a604327
Gopalakrishnan-Subburaman <krishnangopalgk1997@gmail.com>
2024-10-13 22:22:18 +0530

    Add files via upload

---
+++ HomeAssignmentsDay1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1.HomeAssignmentsDay1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			float version = 91.0f;
+			String developer = "Google";
+			boolean isBeta = false;
+			int releaseYear = 1998;
+			char shortcutKey = 'c';
+			
+			System.out.println("Home Assignment Day 1"  );
+			System.out.println("---------------------"  );
+			System.out.println("Chrome is working " +version+ " version"  );
+			System.out.println("Chrome was developed by " +developer);
+			System.out.println("Current Chrome Browser is Beta Version? - " +isBeta);
+			System.out.println("Chrome was released on " +releaseYear);
+			System.out.println("Shortcut to open Chrome Browser is - " +shortcutKey);
+	}
+
+}
